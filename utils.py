a2c9aeb267958cfaa633cade24115e155945d761
Stanisław Wasilewski <wasilewski.sf@gmail.com>
2023-05-11 15:33:09 +0200

    created dummy footer

--- utils.py
+++ utils.py
@@ -25,3 +25,29 @@ def make_header(root):
     lbl_test3 = tk.Label(header, text="BooooOOMER", bg="yellow")
     lbl_test3.grid(row = 0, column=2)
     return header_place
+
+
+def make_footer(root):
+    '''
+    Creates a place for a for a footer (lower menu) and fills it up with a grid.
+    To use it, use: 
+    footer = make_footer(the window in which the header should be)
+    And then, to place it and make it spread to the whole width:
+    footer.pack(fill='x')
+
+
+    '''
+    header_place = tk.Frame(root,
+                  borderwidth=1,
+                  relief="sunken",
+                  bg="yellow")
+    header = tk.Frame(header_place)
+    header.pack()
+    lbl_test = tk.Label(header, text="BOOMER", bg="yellow")
+    lbl_test.config(text = "DOOMER")
+    lbl_test.grid(row=0, column=0)
+    lbl_test2 = tk.Label(header, text="BooooOOMER", bg="yellow")
+    lbl_test2.grid(row = 0, column=1)
+    lbl_test3 = tk.Label(header, text="BooooOOMER", bg="yellow")
+    lbl_test3.grid(row = 0, column=2)
+    return header_place
