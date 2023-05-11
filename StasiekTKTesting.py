fc99590ed3c5a8d5277d7e49d235db62decfbad4
Stanisław Wasilewski <wasilewski.sf@gmail.com>
2023-05-11 15:30:07 +0200

    Moved header to utils, created icons

--- StasiekTKTesting.py
+++ StasiekTKTesting.py
@@ -1,29 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
+from utils import*
 
 root = tk.Tk()
 root.title("Boomer")
 root.geometry('360x640+50+50')
 root.resizable(False,False)
 
-def make_header():
-    header_place = tk.Frame(root,
-                  borderwidth=1,
-                  relief="sunken",
-                  bg="yellow")
-    header = tk.Frame(header_place)
-    header.pack()
-    lbl_test = tk.Label(header, text="BooooOOMER", bg="yellow")
-    lbl_test.config(text = "DOOMER")
-    lbl_test.grid(row=0, column=0)
-    lbl_test2 = tk.Label(header, text="BooooOOMER", bg="yellow")
-    lbl_test2.grid(row = 0, column=1)
-    lbl_test3 = tk.Label(header, text="BooooOOMER", bg="yellow")
-    lbl_test3.grid(row = 0, column=2)
-    return header_place
-
-
-header = make_header()
+header = make_header(root)
 header.pack(fill='x')
 body = tk.Frame(root)
 footer = tk.Frame(root,
